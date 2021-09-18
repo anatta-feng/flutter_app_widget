113890512a9cab8a27fe8ff793db4d7ef9b65a65
渣渣辉 <1@fxcdev.com>
2021-09-18 17:20:08 +0800

    update project

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_app_widget
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAppWidgetPlugin.register(with: registry.registrar(forPlugin: "FlutterAppWidgetPlugin"))
+}
